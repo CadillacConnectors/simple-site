06924521a49a1ef1b4900d131ac51e8f88a39b70
Josh <fudbuds2@gmail.com>
2017-01-14 16:34:19 -0500

    Added footer

--- resources.php
+++ resources.php
@@ -32,4 +32,19 @@ class resources
 
         echo "<header><img src='/images/logo.png' class='full' alt='Cadillac Connectors'><h1>FRC TEAM 5086</h1></header>";
     }
+
+    static function partB() {
+        echo "<footer>";
+        echo "<h2><i>Connect</i> with us</h2>";
+        echo "<p>";
+        echo "    <a href='https://twitter.com/team5086'><img src=\"images/twitter.png\" alt=\"Twitter\" title=\"Cadillac Connectors Twitter\" class=\"mediaButton\"></a>";
+        echo "    <a href='https://facebook.com/team5086'><img src=\"images/facebook.png\" alt=\"Facebook\" title=\"Cadillac Connectors Facebook\" class=\"mediaButton\">";
+        echo "    <a href='https://instagram.com/team5086'><img src=\"images/instagram.png\" alt=\"Instagram\" title=\"Cadillac Connectors Instagram\" class=\"mediaButton\">";
+        echo "    <a href='https://pinterest.com/team5086'><img src=\"images/pintrest.png\" alt=\"Pinterest\" title=\"Cadillac Connectors Pinterest\" class=\"mediaButton\">";
+        echo "    <a href='https://reddit.com/r/cadillacconnectors'><img src=\"images/reddit.png\" alt=\"Reddit\" title=\"Cadillac Connectors Subreddit\" class=\"mediaButton\">";
+        echo "    <a href='https://www.youtube.com/channel/UCAPJa_mUyGjpRXvBHV3ULNA'><img src=\"images/youtube.png\" alt=\"Youtube\" title=\"Cadillac Connectors Youtube Channel\" class=\"mediaButton\">";
+        echo "</p>";
+        echo "<p>&copy;Joshua Jacobson, 2017.</p>";
+        echo "</footer>";
+    }
 }
