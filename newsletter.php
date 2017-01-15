3353efba092bbe5e82e1a7547037127baa4f1099
Josh <fudbuds2@gmail.com>
2017-01-15 16:02:49 -0500

    Added newsletter

--- newsletter.php
+++ newsletter.php
@@ -3,5 +3,29 @@
  * Created by PhpStorm.
  * User: josh
  * Date: 1/13/17
- * Time: 3:45 PM
+ * Time: 3:44 PM
  */
+require 'resources.php';
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>Newsletter</title>
+    <?php resources::header(); ?>
+</head>
+<body>
+
+<?php resources::partA() ?>
+
+<section id="content">
+    <h2>Cadillac Connectors Newsletter:</h2>
+    <h3>2017 -</h3>
+    <p>
+        Issue 1 - out soon
+    </p>
+</section>
+
+<?php resources::partB() ?>
+
+</body>
+</html>
