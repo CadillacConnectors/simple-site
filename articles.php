ad7fd4d89d704f3f4dd70bf9701cb04ef047d639
Josh <fudbuds2@gmail.com>
2017-01-15 16:11:32 -0500

    Added blog

---
+++ articles.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: joshua
+ * Date: 12/31/16
+ * Time: 11:59 AM
+ */
+class articles
+{
+    //Increment this number everytime a new article is created
+    public static $articles = 0;
+
+    static function parseId($id) {
+        $articles = self::$articles;
+        if($id == "latest" || $id > $articles) {
+            $id = $articles;
+        }
+        if ($id < 0) {
+            $id = 0;
+        }
+        if(!is_numeric($id) || strrpos($id, ".")) {
+            $id = $articles;
+        }
+        return $id;
+    }
+
+    static function getArticle($id) {
+
+
+        //Do not change this
+        $id = self::parseId($id);
+        $author = "";
+        $date = "";
+        $title = "";
+        $paragraphs = new ArrayObject();
+        $images = new ArrayObject();
+
+        //Add cases to this for every new article
+        switch ($id) {
+
+            //Article Number 0
+            case 0:
+                //Author's name
+                $author = "Joshua Jacobson";
+
+                //Date that article was published
+                $date = "12/31/2016";
+
+                //Title of article (should be less than 50 characters, doesn't have to be)
+                $title = "Old articles are going to be archived";
+
+                //Add paragraphs like this
+                $paragraphs->append('The old articles from the 2016 season are set to be archieved in  location to be determined.');
+                $paragraphs->append('They will be available shortly.');
+
+                //Add Images like this
+                $images->append("01.png");
+                //They will be search for in /blog/articles/images/YYYY/MM/DD/
+
+                break;
+        }
+
+        //Do not change this
+        self::printArticle($author, self::dateFormatter($date, "date"), self::dateFormatter($date, "humandate"), $date, $title, $paragraphs, $images, self::dateFormatter($date, "folder"));
+    }
+
+    static function printArticle($author, $date, $longdate, $humandate, $title, $paragraphs, $images, $folder) {
+        echo "<article>";
+        echo "<h1>$title</h1>";
+        echo "<address class=\"author\">" . $author . " on <time datetime='$date' title='$longdate'>$humandate</time></address>";
+        foreach ($paragraphs as $p) {
+            echo "<p>" . $p . "</p>";
+        }
+        foreach ($images as $i) {
+            echo '<img src="/blog/articles/images/' . $folder . $i . '" id="' . $i . '" class="image" onclick="expandImage(this)" >';
+        }
+        echo "</article>";
+    }
+
+    static function dateFormatter($date, $type) {
+        if($type=="date") {
+            return date('Y-m-d', strtotime($date));
+        }
+        if($type=="humandate") {
+            return date('M jS, Y', strtotime($date));
+        }
+        if($type=="folder") {
+            return date("Y/m/d/", strtotime($date));
+        }
+        return null;
+    }
+}
--- blog.php
+++ blog.php
@@ -1,7 +1,78 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: josh
- * Date: 1/13/17
- * Time: 3:45 PM
- */
+require 'resources.php';
+$article = $_GET["article"];
+require 'articles.php';
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>Blog</title>
+    <?php resources::header(); ?>
+</head>
+<body>
+
+<?php resources::partA() ?>
+
+<section id="content">
+    <?php
+
+    articles::getArticle($article);
+
+    echo "<br>";
+    echo '<button id="latest" onclick="latest()">Latest</button>';
+    echo '<button id="next" onclick="next(' . $article . ')">Next</button>';
+    echo '<button id="previous" onclick="previous(' . $article . ')">Previous</button>';
+    echo '<button id="first" onclick="first()">First</button>';
+
+    ?>
+</section>
+
+<?php resources::partB() ?>
+
+<script>
+    function next(current) {
+        var url = window.location.href;
+        if(url.indexOf("/articles/") != -1) {
+            url = url.substring(0, url.indexOf("/articles/"));
+        }
+        console.log(url);
+        window.location.href = url + "/articles/index.php?article=" + (current + 1);
+    }
+
+    function previous(current) {
+        var url = window.location.href;
+        if(url.indexOf("/articles/") != -1) {
+            url = url.substring(0, url.indexOf("/articles/"));
+        }
+        console.log(url);
+        window.location.href = url + "/articles/index.php?article=" + (current - 1);
+    }
+
+    function latest() {
+        var url = window.location.href;
+        if(url.indexOf("/articles/") != -1) {
+            url = url.substring(0, url.indexOf("/articles/"));
+        }
+        console.log(url);
+        window.location.href = url + "/articles/index.php?article=latest";
+    }
+
+    function first() {
+        var url = window.location.href;
+        if(url.indexOf("/articles/") != -1) {
+            url = url.substring(0, url.indexOf("/articles/"));
+        }
+        console.log(url);
+        window.location.href = url + "/articles/index.php?article=0";
+    }
+
+    function expandImage(e) {
+        var a = document.getElementsByClassName("largeImage");
+        for(var b in a) {
+            b.className = "image";
+        }
+        e.className = "largeImage";
+    }
+</script>
+</body>
+</html>
