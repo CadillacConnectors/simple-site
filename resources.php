58501f9956503c2334a26c00dcab3c473285b76d
JoshuaJacobson <fudbuds@gmail.com>
2017-02-03 04:55:23 -0500

    Fixed lack of images on blog pages

--- resources.php
+++ resources.php
@@ -11,8 +11,8 @@ class resources
     static function header() {
         echo "<meta charset=\"UTF-8\">";
         echo "<link rel=\"stylesheet\" type=\"text/css\" href=\"/index.css\">";
-        echo "<link rel=\"icon\" href=\"favicon.ico\" type=\"image/x-icon\">";
-        echo "<link rel=\"shortcut icon\" href=\"favicon.ico\" type=\"image/x-icon\">";
+        echo "<link rel=\"icon\" href=\"/favicon.ico\" type=\"image/x-icon\">";
+        echo "<link rel=\"shortcut icon\" href=\"/favicon.ico\" type=\"image/x-icon\">";
     }
 
     static function partA() {
@@ -37,11 +37,11 @@ class resources
         echo "<footer>";
         echo "<h2><i>Connect</i> with us</h2>";
         echo "<p>";
-        echo "    <a href='https://twitter.com/team5086'><img src=\"images/twitter.png\" alt=\"Twitter\" title=\"Cadillac Connectors Twitter\" class=\"mediaButton\"></a>";
-        echo "    <a href='https://facebook.com/team5086'><img src=\"images/facebook.png\" alt=\"Facebook\" title=\"Cadillac Connectors Facebook\" class=\"mediaButton\"></a>";
-        echo "    <a href='https://instagram.com/team5086'><img src=\"images/instagram.png\" alt=\"Instagram\" title=\"Cadillac Connectors Instagram\" class=\"mediaButton\"></a>";
-        echo "    <a href='https://pinterest.com/team5086'><img src=\"images/pintrest.png\" alt=\"Pinterest\" title=\"Cadillac Connectors Pinterest\" class=\"mediaButton\"></a>";
-        echo "    <a href='https://reddit.com/r/cadillacconnectors'><img src=\"images/reddit.png\" alt=\"Reddit\" title=\"Cadillac Connectors Subreddit\" class=\"mediaButton\"></a>";
+        echo "    <a href='https://twitter.com/team5086'><img src=\"/images/twitter.png\" alt=\"Twitter\" title=\"Cadillac Connectors Twitter\" class=\"mediaButton\"></a>";
+        echo "    <a href='https://facebook.com/team5086'><img src=\"/images/facebook.png\" alt=\"Facebook\" title=\"Cadillac Connectors Facebook\" class=\"mediaButton\"></a>";
+        echo "    <a href='https://instagram.com/team5086'><img src=\"/images/instagram.png\" alt=\"Instagram\" title=\"Cadillac Connectors Instagram\" class=\"mediaButton\"></a>";
+        echo "    <a href='https://pinterest.com/team5086'><img src=\"/images/pintrest.png\" alt=\"Pinterest\" title=\"Cadillac Connectors Pinterest\" class=\"mediaButton\"></a>";
+        echo "    <a href='https://reddit.com/r/cadillacconnectors'><img src=\"/images/reddit.png\" alt=\"Reddit\" title=\"Cadillac Connectors Subreddit\" class=\"mediaButton\"></a>";
         echo "    <a href='https://www.youtube.com/channel/UCAPJa_mUyGjpRXvBHV3ULNA'><img src=\"images/youtube.png\" alt=\"Youtube\" title=\"Cadillac Connectors Youtube Channel\" class=\"mediaButton\"></a>";
         echo "</p>";
         echo "<p>&copy;Joshua Jacobson, 2017.</p>";
