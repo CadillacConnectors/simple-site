9643f481d50ac3ceb3e34e8ece8fe9eee3214826
JoshuaJacobson <fudbuds@gmail.com>
2017-03-07 20:34:31 -0500

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,7 +3,6 @@ $target_dir = "uploads/";
 $target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
 $uploadOk = 1;
 $imageFileType = pathinfo($target_file,PATHINFO_EXTENSION);
-$imageFileType = strtolower($imageFileType);
 // Check if image file is a actual image or fake image
 //if(isset($_POST["submit"])) {
 //    $check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
@@ -16,21 +15,21 @@ $imageFileType = strtolower($imageFileType);
 //    }
 //}
 // Check if file already exists
-if (file_exists($target_file)) {
-    echo "Sorry, file already exists. ";
-    $uploadOk = 0;
-}
+//if (file_exists($target_file)) {
+//    echo "Sorry, file already exists. ";
+//    $uploadOk = 0;
+//}
 // Check file size
 if ($_FILES["fileToUpload"]["size"] > 5000000) {
     echo "Sorry, your file is too large. ";
     $uploadOk = 0;
 }
 // Allow certain file formats
-//if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-//&& $imageFileType != "gif" ) {
-//    echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed. ";
-//    $uploadOk = 0;
-//}
+if($imageFileType == "php" && $imageFileType == "html" && $imageFileType == "htm"
+&& $imageFileType == "exe" ) {
+    echo "Sorry, only Image or video files are allowed. ";
+    $uploadOk = 0;
+}
 // Check if $uploadOk is set to 0 by an error
 if ($uploadOk == 0) {
     echo "Sorry, your file was not uploaded. ";
