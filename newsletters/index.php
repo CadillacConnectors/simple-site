453e85307f5b68b961be1f165fb802ac7f7f7d42
JoshuaJacobson <fudbuds@gmail.com>
2017-04-17 06:24:25 -0400

    Fixing broken links

--- newsletters/index.php
+++ newsletters/index.php
@@ -1,7 +1,7 @@
 <?php
 $issue = $_GET['i'];
 if ($issue < 10) {
-    header( 'Location: https://www.connectors5086.org/newsletters/Issue%200' . $issue . '.pdf') ;
+    header( 'Location: https://connectors5086.org/newsletters/Issue%200' . $issue . '.pdf') ;
 } else {
-    header( 'Location: https://www.connectors5086.org/newsletters/Issue%20' . $issue . '.pdf') ;
+    header( 'Location: https://connectors5086.org/newsletters/Issue%20' . $issue . '.pdf') ;
 }
