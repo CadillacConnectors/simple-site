b5791f126b2bb2e824fa7e788070ae96c7bf60ca
JoshuaJacobson <fudbuds@gmail.com>
2017-04-17 06:16:15 -0400

    Added pdf tags

--- newsletters/index.php
+++ newsletters/index.php
@@ -1,7 +1,7 @@
 <?php
 $issue = $_GET['i'];
 if ($issue < 10) {
-    header( 'Location: https://www.connectors5086.org/newsletters/Issue%200' . $issue ) ;
+    header( 'Location: https://www.connectors5086.org/newsletters/Issue%200' . $issue . '.pdf') ;
 } else {
-    header( 'Location: https://www.connectors5086.org/newsletters/Issue%20' . $issue ) ;
+    header( 'Location: https://www.connectors5086.org/newsletters/Issue%20' . $issue . '.pdf') ;
 }
