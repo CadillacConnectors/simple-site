a72360cc67c534ee90333974928eb97d30f8f934
JoshuaJacobson <fudbuds@gmail.com>
2017-04-17 06:07:28 -0400

    Add newsletters 4-8

--- newsletter.php
+++ newsletter.php
@@ -21,13 +21,28 @@ require 'resources.php';
     <h2>Cadillac Connectors Newsletter:</h2>
     <h3>2017 -</h3>
     <p>
-        Issue 1 - <a href="newsletters/Issue%2001.pdf">Download here</a>.
+        Issue 1 - <a href="newsletters/Issue%2001.pdf">1/13/2017</a>.
     </p>
     <p>
-        Issue 2 - <a href="newsletters/Issue%2002.pdf">Download here</a>.
+        Issue 2 - <a href="newsletters/Issue%2002.pdf">1/27/2017</a>.
     </p>
     <p>
-        Issue 3 - <a href="newsletters/Issue%2003.pdf">Download here</a>.
+        Issue 3 - <a href="newsletters/Issue%2003.pdf">2/11/2017</a>.
+    </p>
+    <p>
+        Issue 4 - <a href="newsletters/Issue%2004.pdf">2/25/2017</a>.
+    </p>
+    <p>
+        Issue 5 - <a href="newsletters/Issue%2005.pdf">3/11/2017</a>.
+    </p>
+    <p>
+        Issue 6 - <a href="newsletters/Issue%2006.pdf">2/25/2017</a>.
+    </p>
+    <p>
+        Issue 7 [SPECIAL EDITION]- <a href="newsletters/Issue%2007.pdf">4/11/2017</a>.
+    </p>
+    <p>
+        Issue 8 - <a href="newsletters/Issue%2008.pdf">4/17/2017</a>.
     </p>
 </section>
 
