62e2d57751403a4d69f6b00eb42089bfb76d8067
JoshuaJacobson <fudbuds@gmail.com>
2017-04-16 21:44:24 -0400

    Create donate.php

---
+++ donate.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by Github
+ * User: josh
+ * Date: 4/16/17
+ * Time: 9:41 PM
+ */
+require 'resources.php';
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>Donations</title>
+    <?php resources::header(); ?>
+</head>
+<body>
+
+<?php resources::partA() ?>
+
+<section id="content">
+    <h2>Donation Process</h2>
+    <p>
+        Are you a community member insterested in supporting the cadillac connectors wth a small donation?
+        We are always looking for community support to help our team perform the best we can!
+        Contact us at <a href="mailto:frcteam5086@gmail.com" target="_top">frcteam5086@gmail.com</a> to get started.
+    </p>
+</section>
+
+<?php resources::partB() ?>
+
+</body>
+</html>
