aa1728b41724b89483ebcd9530a8a2c7885e55e4
JoshuaJacobson <fudbuds@gmail.com>
2017-04-17 06:15:09 -0400

    I'm an idiot

--- newsletters/index.php
+++ newsletters/index.php
@@ -1,6 +1,6 @@
 <?php
 $issue = $_GET['i'];
-if ($switch < 10) {
+if ($issue < 10) {
     header( 'Location: https://www.connectors5086.org/newsletters/Issue%200' . $issue ) ;
 } else {
     header( 'Location: https://www.connectors5086.org/newsletters/Issue%20' . $issue ) ;
