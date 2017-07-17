697c34414d0690d51ea07453ab7943b8b132fe82
JoshuaJacobson <fudbuds@gmail.com>
2017-07-17 01:05:24 -0400

    Create stematthey.php

---
+++ stematthey.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * Created by Github
+ * User: josh
+ * Date: 7/17/17
+ * Time: 12:39 AM
+ */
+require 'resources.php';
+?>
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+    <title>Summer Stem Days</title>
+    <?php resources::header(); ?>
+</head>
+<body>
+
+<?php resources::partA() ?>
+
+<section id="content">
+    <h2>Announcing Summer Stem Day Programs at the YMCA</h2>
+    <p>
+        The connectors are proud to be a part of a <b>brand new</b> program at the YMCA for students going into grades 5 through 8 for 
+        five weeks, starting on <i>July 24<sup>th</sup></i> and going on every <i>Monday</i> and <i>Tuesday</i> until <i>August 
+        22<sup>nd</sup></i>. The program features cool demonstrations in various areas from Suprising Science Reactions 
+        to Video Game Programming and will take place from <b>3:30</b> to </b>5:30</b> PM. Want more deatails? Check out 
+        <a href="//ymca.pdf">this flyer</a>.
+    </p>
+</section>
+
+<?php resources::partB() ?>
+
+</body>
+</html>
