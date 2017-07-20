66edcc99d809a910eb9ee619970e9fa24aa128ac
JoshuaJacobson <fudbuds@gmail.com>
2017-07-19 21:36:06 -0400

    Added data mining

--- newsletters/index.php
+++ newsletters/index.php
@@ -1,5 +1,13 @@
 <?php
 $issue = $_GET['i'];
+$ip = $_SERVER['REMOTE_ADDR'];
+$date = new DateTime();
+
+$log = fopen("issue$issue.txt", "a+");
+$value = "$ip\v$date\n";
+fwrite($log, $value);
+fclose($log);
+
 if ($issue < 10) {
     header( 'Location: https://connectors5086.org/newsletters/Issue%200' . $issue . '.pdf') ;
 } else {
