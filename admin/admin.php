e774ae7a4b01ae333237e4b22de32e3dd0715d3a
JoshuaJacobson <fudbuds@gmail.com>
2017-08-27 00:21:51 -0400

    removing telemetry

--- admin/admin.php
+++ admin/admin.php
@@ -38,8 +38,7 @@ function verify($username, $password) {
         return "timeout";
     }
     foreach(file("/var/www/users.conf") as $line) {
-        echo $line . " = " . $combination . " " . strcasecmp($line, $combination);
-        if (strcasecmp($line, $combination) == 0) {
+        if (strcasecmp($line, $combination) == 1) {
             return "verified";
         }
     }
