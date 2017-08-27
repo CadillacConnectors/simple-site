8550251f8d85cd9c64c483be92dc0b242a648385
JoshuaJacobson <fudbuds@gmail.com>
2017-08-27 00:05:51 -0400

    Bugfix strcasecmp

--- admin/admin.php
+++ admin/admin.php
@@ -39,7 +39,7 @@ function verify($username, $password) {
     }
     foreach(file("/var/www/users.conf") as $line) {
         echo $line . " = " . $combination;
-        if (strcasecmp($line, $combination)) {
+        if (strcasecmp($line, $combination) == 0) {
             return "verified";
         }
     }
